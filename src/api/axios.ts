7264e52615409e5e74a2c3a72842b62508529544
Ishwor Giri <i.giri@elipzis.com>
2023-09-16 00:46:41 +0200

    Proj Init

---
+++ src/api/axios.ts
@@ -0,0 +1,14 @@
+import baseAxios from "axios";
+
+const API_URL = "https://catfact.ninja";
+
+export const api = baseAxios.create({
+  baseURL: API_URL,
+  timeout: 1000,
+  withCredentials: false,
+  headers: { "X-Frontend": true, "Access-Control-Allow-Origin": true },
+});
+
+export async function getEntries() {
+  return (await api.get("/fact")).data;
+}
