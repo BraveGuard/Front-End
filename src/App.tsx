64efeb81bf3e0cd6f01daa5a562b4cf7bb222097
Ishwor Giri <i.giri@elipzis.com>
2023-09-16 17:12:10 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
         <div className="h-full flex flex-col bg-whites ">
           <BrowserRouter>
             <motion.div
-              className="flex-1 overflow-y-scroll  py-2"
+              className="flex-1 overflow-y-scroll  "
               layout
               transition={{ duration: 1, type: "spring" }}
             >
